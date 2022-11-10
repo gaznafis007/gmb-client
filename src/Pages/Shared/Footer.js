cead0f1cce5d3d2e38a186efee65743be67d93b1
gazinafis007 <gazinafisrafi.gnr@gmail.com>
2022-11-10 21:18:54 +0600

    footer done

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>This is footer</h2>
-    </div>
+    <footer className="footer footer-center p-10 bg-base-300 text-content">
+      <div>
+        <p className="font-bold">
+          DMB AKASH PHOTOGRAPHY <br />
+          Providing reliable tech since 2020
+        </p>
+        <p>Copyright © 2022 - All right reserved</p>
+      </div>
+      <div>
+        <div className="grid grid-flow-col gap-4">
+          <a>
+            <FaTwitter className="text-2xl" />
+          </a>
+          <a>
+            <FaYoutube className="text-2xl" />
+          </a>
+          <a>
+            <FaFacebook className="text-2xl" />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
